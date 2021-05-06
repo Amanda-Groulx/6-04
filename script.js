96afdaf1d67143342fdc0f12e72c7aeb052a910b
AmandaGroulx <>
2021-05-06 17:40:04 +0000

    added spaces js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let number = 0
 document.getElementById('calculate').addEventListener('click', buttonOne)
 
 function buttonOne () {
-    height = document.getElementById('height').value
-    height = parseInt(height)
-    baseOne = document.getElementById('base-one').value
-    baseOne = parseInt(baseOne)
-    baseTwo = document.getElementById('base-two').value
-    baseTwo = parseInt(baseTwo)
-    number = (baseOne + baseTwo) / 2 * height
-    document.getElementById('area').innerHTML = (number)
+  height = document.getElementById('height').value
+  height = parseInt(height)
+  baseOne = document.getElementById('base-one').value
+  baseOne = parseInt(baseOne)
+  baseTwo = document.getElementById('base-two').value
+  baseTwo = parseInt(baseTwo)
+  number = (baseOne + baseTwo) / 2 * height
+  document.getElementById('area').innerHTML = (number)
 }
